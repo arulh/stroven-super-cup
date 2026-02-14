4f574fe8b1bc53c7e8133c2abac8606646b816dd
arulh <arul.howard@telus.net>
2026-02-13 22:15:44 -0800

    update winner and stats

--- frontend/src/components/Champions.tsx
+++ frontend/src/components/Champions.tsx
@@ -12,7 +12,7 @@ const Champions: React.FC = () => {
   const theme = useTheme();
 
   const champions: Champion[] = [
-    { name: "Niko", wins: 11 },
+    { name: "Niko", wins: 12 },
     { name: "Joel", wins: 1 },
     { name: "Arul", wins: 1 },
   ];
